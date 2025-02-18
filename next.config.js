378b856d918cd90c6bc92508de344d7fac0840ad
Anish Nyayachavadi <anishnya@umich.edu>
2025-02-18 00:39:39 -0800

    goodbye seattle - owl city fr

--- next.config.js
+++ next.config.js
@@ -1,10 +1,17 @@
-const withOffline = require('next-offline');
-
+/**
+ * @type {import('next').NextConfig}
+ */
 const nextConfig = {
 	output: 'export',
-	poweredByHeader: false,
-	swcMinify: true,
-};
+	images: { unoptimized: true },
+	// Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
+	// trailingSlash: true,
 
-module.exports = withOffline(nextConfig);
+	// Optional: Prevent automatic `/me` -> `/me/`, instead preserve `href`
+	// skipTrailingSlashRedirect: true,
 
+	// Optional: Change the output directory `out` -> `dist`
+	// distDir: 'dist',
+}
+
+module.exports = nextConfig
